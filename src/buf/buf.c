21b1b464eb78aab41c4c7789d90585dca7c8a1c9
sloweax <sloweax@users.noreply.github.com>
2024-02-22 14:39:36 -0300

    move `qk_buf_read*` to seperate file

--- src/buf/buf.c
+++ src/buf/buf.c
@@ -1,8 +1,6 @@
 #include "../../include/qk/buf.h"
-#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #define MAX(A, B) (A) > (B) ? (A) : (B)
 
@@ -101,30 +99,6 @@ QKAPI int qk_buf_cat(qk_buf *b, const void *data, size_t len)
     memcpy(b->data+b->len, data, len);
     b->len += len;
     return QK_OK;
-}
-
-QKAPI int qk_buf_read(qk_buf *b, int fd)
-{
-    char buf[4096];
-    ssize_t rd;
-    int r;
-
-    while ((rd = read(fd, buf, sizeof(buf)))) {
-        if (rd == -1) return QK_ERRNO;
-        r = qk_buf_cat(b, buf, rd);
-        if (r != QK_OK) return r;
-    }
-
-    return QK_OK;
-}
-
-QKAPI int qk_buf_read_path(qk_buf *b, const char *path)
-{
-    int fd = open(path, O_RDONLY);
-    if (fd == -1) return QK_ERRNO;
-    int r = qk_buf_read(b, fd);
-    if (close(fd) != 0) return r == QK_OK ? QK_ERRNO : r;
-    return r;
 }
 
 QKAPI qk_buf *qk_buf_dup(const qk_buf *b)
