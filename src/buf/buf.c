ff6bc87d1d2bf89fcc65da38ad1f90eb66639817
sloweax <sloweax@users.noreply.github.com>
2024-02-24 13:59:24 -0300

    move `qk_buf_*fit` to fit.c

--- src/buf/buf.c
+++ src/buf/buf.c
@@ -54,26 +54,3 @@ QKAPI int qk_buf_reserve(qk_buf *b, size_t cap)
 
     return qk_buf_grow(b, cap - b->cap);
 }
-
-QKAPI int qk_buf_fit(qk_buf *b)
-{
-    if (b->len == b->cap) return QK_OK;
-
-    if (!(b->flags & QK_BUF_DATA_ALLOC))
-        return QK_INVALID;
-
-    if (b->len == 0) {
-        QK_FREE(b->data);
-        b->data = NULL;
-        b->cap = 0;
-        return QK_OK;
-    }
-
-    void *data = QK_REALLOC(b->data, b->len);
-    if (data == NULL) return QK_ERRNO;
-
-    b->data = data;
-    b->cap = b->len;
-
-    return QK_OK;
-}
--- src/buf/fit.c
+++ src/buf/fit.c
@@ -25,3 +25,26 @@ QKAPI int qk_buf_sfit(qk_buf *b)
 
     return QK_OK;
 }
+
+QKAPI int qk_buf_fit(qk_buf *b)
+{
+    if (b->len == b->cap) return QK_OK;
+
+    if (!(b->flags & QK_BUF_DATA_ALLOC))
+        return QK_INVALID;
+
+    if (b->len == 0) {
+        QK_FREE(b->data);
+        b->data = NULL;
+        b->cap = 0;
+        return QK_OK;
+    }
+
+    void *data = QK_REALLOC(b->data, b->len);
+    if (data == NULL) return QK_ERRNO;
+
+    b->data = data;
+    b->cap = b->len;
+
+    return QK_OK;
+}
