3542ffec534c91e3aaa233835239763ca55384cf
sloweax <sloweax@users.noreply.github.com>
2024-03-12 20:03:51 -0300

    remove `QK_{FREE,MALLOC,REALLOC}`

--- include/qk.h
+++ include/qk.h
@@ -1,15 +1,5 @@
 #pragma once
 
-#ifndef QK_MALLOC
-#define QK_MALLOC(sz) malloc(sz)
-#endif
-#ifndef QK_FREE
-#define QK_FREE(p)    free(p)
-#endif
-#ifndef QK_REALLOC
-#define QK_REALLOC(p, sz)    realloc(p, sz)
-#endif
-
 #ifndef QKAPI
 #define QKAPI extern
 #endif
