ca8565889d6046898e51145040bc11835265f691
sloweax <sloweax@users.noreply.github.com>
2024-03-13 18:27:17 -0300

    fix `qk_hmap_alloc_str` free `oldsz`

--- src/hmap/alloc.c
+++ src/hmap/alloc.c
@@ -7,7 +7,7 @@ QKAPI void *qk_hmap_alloc_str(qk_allocator *ctx, void *p, size_t oldsz, size_t n
     char *s = (char*)p;
     size_t len = strlen(s);
     if (newsz == 0)
-        return ctx->alloc(ctx, p, len, 0);
+        return ctx->alloc(ctx, p, len+1, 0);
     char *ptr = ctx->alloc(ctx, NULL, 0, len+1);
     if (ptr == NULL) return NULL;
     strcpy(ptr, s);
